72aacfb34091347954ea93db618f2a08cc013990
DanielPedraza509 <pedrazajaca2001@gmail.com>
2024-11-15 01:51:14 -0500

    cors

--- src/main/java/com/example/demo/KaraokeApplication.java
+++ src/main/java/com/example/demo/KaraokeApplication.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,7 +18,7 @@ public class KaraokeApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
